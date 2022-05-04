1dc7daa399a92b991ad43e7d58cbedc25c6218c0
atsu0131 <atsushi.isai131@gmail.com>
2022-05-04 22:08:21 +0900

    add

--- config/puma.rb
+++ config/puma.rb
@@ -1,44 +1,22 @@
-# Puma can serve each request in a thread from an internal thread pool.
-# The `threads` method setting takes two numbers: a minimum and maximum.
-# Any libraries that use thread pools should be configured to match
-# the maximum value specified for Puma. Default is set to 5 threads for minimum
-# and maximum; this matches the default thread size of Active Record.
-#
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+
+root_dir = '/var/www/port-backend-api'
+
+max_threads_count = ENV.fetch('RAILS_MAX_THREADS', 5)
+
+min_threads_count = ENV.fetch('RAILS_MIN_THREADS', max_threads_count)
+
 threads min_threads_count, max_threads_count
 
-# Specifies the `worker_timeout` threshold that Puma will use to wait before
-# terminating a worker in development environments.
-#
-worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
+worker_timeout 60
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
-# port ENV.fetch("PORT") { 3000 }
-bind "unix:///var/www/port-backend-api/tmp/sockets/puma.sock"
-# Specifies the `environment` that Puma will run in.
-#
-environment ENV.fetch("RAILS_ENV") { "development" }
+bind "unix://#{root_dir}/tmp/sockets/puma.sock"
 
-# Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+environment 'production'
 
-# Specifies the number of `workers` to boot in clustered mode.
-# Workers are forked web server processes. If using threads and workers together
-# the concurrency of the application would be max `threads` * `workers`.
-# Workers do not work on JRuby or Windows (both of which do not support
-# processes).
-#
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+pidfile File.expand_path('tmp/pids/server.pid')
 
-# Use the `preload_app!` method when specifying a `workers` number.
-# This directive tells Puma to first boot the application and load code
-# before forking the application. This takes advantage of Copy On Write
-# process behavior so workers use less memory.
-#
-# preload_app!
+stdout_redirect File.expand_path('log/puma_access.log'), File.expand_path('log/puma_error.log'), true
 
-# Allow puma to be restarted by `rails restart` command.
+# workerの数は適宜変更する。指定しない場合はsingle modeとなるが、指定した場合はcluster modeとなる。
+
 plugin :tmp_restart
-
