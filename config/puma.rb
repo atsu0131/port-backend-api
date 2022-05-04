73a26c1a618905bd1cd7ec0f24df9e9b80639929
atsu0131 <atsushi.isai131@gmail.com>
2022-05-04 14:03:41 +0900

    add

--- config/puma.rb
+++ config/puma.rb
@@ -15,8 +15,8 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port ENV.fetch("PORT") { 3000 }
-
+# port ENV.fetch("PORT") { 3000 }
+bind "unix:///var/www/port-backend-api/tmp/sockets/puma.sock"
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
@@ -41,3 +41,4 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
