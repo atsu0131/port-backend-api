bbb713bbebda1014947c6ca88229b5de89696804
atsu0131 <atsushi.isai131@gmail.com>
2022-05-06 02:44:16 +0900

    add

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins "localhost:3000", 'https://dashing-crisp-d04d86.netlify.app/' # React側はポート番号3000で作るので「localhost:3000」を指定
+      origins "localhost:3000", 'https://dashing-crisp-d04d86.netlify.app' # React側はポート番号3000で作るので「localhost:3000」を指定
   
       resource "*",
         headers: :any,
